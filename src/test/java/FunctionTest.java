01428819e2c7a9e5b17947e1352457f821f0d235
jakev <jakevotincev@gmail.com>
2021-03-16 18:37:42 +0300

    add parametrized tests

--- src/test/java/FunctionTest.java
+++ src/test/java/FunctionTest.java
@@ -1,50 +1,23 @@
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class FunctionTest {
-    HashMap<Double, Double> trueTestValues = new HashMap<>();
-    HashMap<Double, Double> falseTestValues = new HashMap<>();
     Function function = new Function();
     double DELTA = 0.001;
 
-    @Before
-    public void setUp() {
-        trueTestValues.put(0.0, 0.0);
-        trueTestValues.put(Math.PI / 6, Math.sqrt(3) / 3);
-        trueTestValues.put(-Math.PI / 6, -Math.sqrt(3) / 3);
-        trueTestValues.put(Math.PI / 4, 1.0);
-        trueTestValues.put(-Math.PI / 4, -1.0);
-        //>1
-        falseTestValues.put(Math.PI / 3, Math.sqrt(3));
-        falseTestValues.put(-Math.PI / 3, -Math.sqrt(3));
-        falseTestValues.put(1.326, 4.0);
-        falseTestValues.put(-1.326, -4.0);
+    @ParameterizedTest
+    @CsvFileSource(resources = "/params.csv")
+    void testOnTrueValues(double expected, double value) {
+        double actual = function.arctg(value);
+        Assertions.assertEquals(expected, actual, DELTA, "ОшибОчка вышла: value " + value);
     }
 
-    @Test
-    public void test() {
-        double actual, expected, value;
-        for (Map.Entry<Double, Double> entry : trueTestValues.entrySet()) {
-            value = entry.getValue();
-            expected = entry.getKey();
-            actual = function.arctg(value);
-            Assert.assertEquals("ОшибОчка вышла: value " + value, expected, actual, DELTA);
-        }
-        for (Map.Entry<Double, Double> entry : falseTestValues.entrySet()) {
-            value = entry.getValue();
-            expected = entry.getKey();
-            actual = function.arctg(value);
-            Assert.assertNotEquals("ОшибОчка вышла: value " + value, expected, actual, DELTA);
-        }
+    @ParameterizedTest
+    @CsvFileSource(resources = "/falseparams.csv")
+    void testOnFalseValues(double expected, double value) {
+        double actual = function.arctg(value);
+        Assertions.assertNotEquals(expected, actual, DELTA, "ОшибОчка вышла: value " + value);
     }
 
-    @After
-    public void tearDown() {
-        trueTestValues.clear();
-    }
 }
